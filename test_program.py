7d1d257657f972ee8410ebbacbeff2e457d5b433
Александр Толстиков <rubikonthe@yandex.ru>
2022-12-13 19:45:21 +0300

    Added new  files

--- test_program.py
+++ test_program.py
@@ -8,12 +8,12 @@ files_list = ['program.py', 'readme.md']
 
 def test_program():
     for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
+        assert filename in dir_files, print(f'Файл `{filename}` не найден в корне репозитория')
 
     try:
         import program
     except Exception as e:
-        assert False, (
+        assert False, print(
             'Не удалось запустить `program.py`. '
             'Исправьте в нем ошибки:\n'
             f'{e}'
